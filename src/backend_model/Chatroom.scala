54b0294ad742a2541053e6a8be348dcf4d867efa
jawn <24411983+keithdugan@users.noreply.github.com>
2019-05-05 21:51:58 -0400

    barebones

--- src/backend_model/Chatroom.scala
+++ src/backend_model/Chatroom.scala
@@ -1,6 +1,19 @@
 package backend_model
 
-class Chatroom {
-  //Chatroom class will hold all messages in a data
+import scala.collection.mutable
 
+class Chatroom {
+  //Chatroom class will hold all messages in a linkedlist of string
+  // Displaying all messages will be O(n)
+  // Sending messages O(1)
+  var messages = new mutable.MutableList[String]
+
+  def addMessage(msg: String): Unit = this.messages += msg
+
+  def toOneString(): String = {
+    val delimiter = "~"
+    var sb: mutable.StringBuilder = new mutable.StringBuilder()
+    messages.foreach((s: String) => sb ++= s + delimiter)
+    sb.toString()
+  }
 }
