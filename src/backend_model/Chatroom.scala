2708005f942c116c4ead9b3ba9f93f63f1221dd6
jawn <24411983+keithdugan@users.noreply.github.com>
2019-05-05 21:55:55 -0400

    chatroom barebones

--- src/backend_model/Chatroom.scala
+++ src/backend_model/Chatroom.scala
@@ -6,11 +6,16 @@ class Chatroom {
   //Chatroom class will hold all messages in a linkedlist of string
   // Displaying all messages will be O(n)
   // Sending messages O(1)
+  // Everyone will send their message with their username appended:
+  // Ex: sample mesagge "test" from user: Aladeen
+  // "Aladeen: test"
   var messages = new mutable.MutableList[String]
 
   def addMessage(msg: String): Unit = this.messages += msg
 
-  def toOneString(): String = {
+  def addUserMessage(msg: String, name: String): Unit = this.messages += name + ": " + msg
+
+  def toOneString: String = {
     val delimiter = "~"
     var sb: mutable.StringBuilder = new mutable.StringBuilder()
     messages.foreach((s: String) => sb ++= s + delimiter)
---
+++ src/backend_model/chatter.scala
@@ -0,0 +1,5 @@
+package backend_model
+
+class chatter {
+
+}
